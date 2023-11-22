06fd9ec1049d44257e09bfc0543463f32c964136
Gustavo Minguta <gustavo.minguta@gmail.com>
2023-11-22 09:44:54 -0300

    subindo aula

---
+++ modulo-02/aula-14-string/tipoString.js
@@ -0,0 +1,8 @@
+"use strict";
+//single quotes
+let nomeCompleto = 'Gustavo da Silva Minguta';
+//doubles quotes 
+let funcaoEmpresa = "Front-End Developer";
+// back ticks
+let detalhesEmpregado = `Meu nome é ${nomeCompleto} e eu sou ${funcaoEmpresa}`;
+console.log(detalhesEmpregado);
---
+++ modulo-02/aula-14-string/tipoString.ts
@@ -0,0 +1,14 @@
+ //single quotes
+
+ let nomeCompleto: string = 'Gustavo da Silva Minguta';
+ 
+
+ //doubles quotes 
+ let funcaoEmpresa: string = "Front-End Developer";
+
+
+ // back ticks
+
+ let detalhesEmpregado: string = `Meu nome é ${nomeCompleto} e eu sou ${funcaoEmpresa}`;
+
+ console.log(detalhesEmpregado);
